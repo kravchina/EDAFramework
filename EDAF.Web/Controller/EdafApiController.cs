57d4b326fc2ccaa6acf8859bdf4143985d716f27
Slava K <sik2005@yandex.ru>
2013-04-18 01:06:53 +0300

    Added base api controller

--- EDAF.Web/Controller/EdafApiController.cs
+++ EDAF.Web/Controller/EdafApiController.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
+using System.Web.Http;
 
 namespace EDAF.Web.Controller
 {
-    class EdafApiController
+    public class EdafApiController<TPrincipal> : ApiController where TPrincipal : class, IPrincipal 
     {
+        public new TPrincipal User
+        {
+            get { return base.User as TPrincipal; }
+        }
     }
 }
